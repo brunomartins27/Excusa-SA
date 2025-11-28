0ce5bb2768ec6478644d72ce219ffe03d924ac30
Marcus Melo <marcus.melo@davinci.edu.ar>
2025-11-28 15:39:13 -0300

    Removendo pasta api duplicada para limpar estrutura

--- api/src/main/java/com/excusas/api/repository/ProntuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.excusas.api.repository;
-
-import com.excusas.api.model.Prontuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProntuarioRepository extends JpaRepository<Prontuario, Long> {
-}
